9bfb93e32e88a598a9d17f82f253b212e26747e1
kaktyshiny <kaktyshiny@gmail.com>
2017-11-30 13:44:47 +0300

    first test

--- homework10/weather/tests.py
+++ homework10/weather/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
+import requests
 
 # Create your tests here.
 
 
-# class TestViews(TestCase):
-#     def test:
-#         pass
+class TestViews(TestCase):
+    def test_get_api(self):
+        response = self.client.get('/weather/')
+        self.assertEquals(response.status_code, 200)
