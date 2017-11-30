d732c929d243ea7bbd72a44e95b1c959e334b195
kaktyshiny <kaktyshiny@gmail.com>
2017-11-30 13:55:05 +0300

    fix 2

--- homework10/weather/tests.py
+++ homework10/weather/tests.py
@@ -1,12 +1,8 @@
 from django.test import TestCase
-import pytest
-import requests
 
 # Create your tests here.
 
-def test_get_api():
-    from weather.views import getAPI
-    url = "http://api.openweathermap.org/data/2.5/find?q=Moscow,RU&APPID=4611941f0036a7d122cc88436e7e4940"
-    answer = getAPI(url)
-    right_api = requests.get(url)
-    assert right_api == answer
+class TestViews(TestCase):
+    def test_page_is_working(self):
+        response = self.client.get('/weather/')
+        self.assertEquals(response.status_code, 200)
